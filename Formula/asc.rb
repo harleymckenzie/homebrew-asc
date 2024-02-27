264428fda39d50a6dc3ad74a110e60ef7a332212
GitHub Action <action@github.com>
2024-02-27 10:06:40 +0000

    Update formula for v0.0.1

--- Formula/asc.rb
+++ Formula/asc.rb
@@ -2,7 +2,7 @@ class Asc < Formula
     desc "AWS Simple CLI (asc)"
     homepage "https://github.com/harleymckenzie/asc"
     url "https://api.github.com/repos/harleymckenzie/asc/tarball/v0.0.1"
-    sha256 "65f5c310cdf73148843397b32e66957da8763544f065162df4651792b902cb66"
+    sha256 "38e394765d3db44f5101a8315d3b8e68716d1ad8130a90baa3b79f702d2a7d9d"
   
     depends_on "python@3.11"
   
