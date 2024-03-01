575fb9ed00eed962b0780459d7f29cc8aec6f5db
GitHub Action <action@github.com>
2024-03-01 23:14:48 +0000

    Update formula for v0.1.0

--- Formula/asc.rb
+++ Formula/asc.rb
@@ -3,54 +3,54 @@ class Asc < Formula
 
   desc "AWS Simple CLI (asc)"
   homepage "https://github.com/harleymckenzie/asc"
-  url "https://api.github.com/repos/harleymckenzie/asc/tarball/v0.4.3"
-  sha256 "b4fe11d045acdf5e59a82219c05273309e6549085624845464aa33e54ada22fa"
+  url "https://api.github.com/repos/harleymckenzie/asc/tarball/v0.1.0"
+  sha256 "9c3c2a4ad674243e2b81b502eaab7c372f02e8c39df949d7c4804733ce7884f1"
   
   depends_on "python@3.11"
 
   resource "boto3" do
-    url "https://api.github.com/repos/harleymckenzie/asc/tarball/v0.4.3"
-    sha256 "b4fe11d045acdf5e59a82219c05273309e6549085624845464aa33e54ada22fa"
+    url "https://api.github.com/repos/harleymckenzie/asc/tarball/v0.1.0"
+    sha256 "9c3c2a4ad674243e2b81b502eaab7c372f02e8c39df949d7c4804733ce7884f1"
   end
 
   resource "botocore" do
-    url "https://api.github.com/repos/harleymckenzie/asc/tarball/v0.4.3"
-    sha256 "b4fe11d045acdf5e59a82219c05273309e6549085624845464aa33e54ada22fa"
+    url "https://api.github.com/repos/harleymckenzie/asc/tarball/v0.1.0"
+    sha256 "9c3c2a4ad674243e2b81b502eaab7c372f02e8c39df949d7c4804733ce7884f1"
   end
 
   resource "configparser" do
-    url "https://api.github.com/repos/harleymckenzie/asc/tarball/v0.4.3"
-    sha256 "b4fe11d045acdf5e59a82219c05273309e6549085624845464aa33e54ada22fa"
+    url "https://api.github.com/repos/harleymckenzie/asc/tarball/v0.1.0"
+    sha256 "9c3c2a4ad674243e2b81b502eaab7c372f02e8c39df949d7c4804733ce7884f1"
   end
 
   resource "jmespath" do
-    url "https://api.github.com/repos/harleymckenzie/asc/tarball/v0.4.3"
-    sha256 "b4fe11d045acdf5e59a82219c05273309e6549085624845464aa33e54ada22fa"
+    url "https://api.github.com/repos/harleymckenzie/asc/tarball/v0.1.0"
+    sha256 "9c3c2a4ad674243e2b81b502eaab7c372f02e8c39df949d7c4804733ce7884f1"
   end
 
   resource "python-dateutil" do
-    url "https://api.github.com/repos/harleymckenzie/asc/tarball/v0.4.3"
-    sha256 "b4fe11d045acdf5e59a82219c05273309e6549085624845464aa33e54ada22fa"
+    url "https://api.github.com/repos/harleymckenzie/asc/tarball/v0.1.0"
+    sha256 "9c3c2a4ad674243e2b81b502eaab7c372f02e8c39df949d7c4804733ce7884f1"
   end
 
   resource "s3transfer" do
-    url "https://api.github.com/repos/harleymckenzie/asc/tarball/v0.4.3"
-    sha256 "b4fe11d045acdf5e59a82219c05273309e6549085624845464aa33e54ada22fa"
+    url "https://api.github.com/repos/harleymckenzie/asc/tarball/v0.1.0"
+    sha256 "9c3c2a4ad674243e2b81b502eaab7c372f02e8c39df949d7c4804733ce7884f1"
   end
 
   resource "six" do
-    url "https://api.github.com/repos/harleymckenzie/asc/tarball/v0.4.3"
-    sha256 "b4fe11d045acdf5e59a82219c05273309e6549085624845464aa33e54ada22fa"
+    url "https://api.github.com/repos/harleymckenzie/asc/tarball/v0.1.0"
+    sha256 "9c3c2a4ad674243e2b81b502eaab7c372f02e8c39df949d7c4804733ce7884f1"
   end
 
   resource "tabulate" do
-    url "https://api.github.com/repos/harleymckenzie/asc/tarball/v0.4.3"
-    sha256 "b4fe11d045acdf5e59a82219c05273309e6549085624845464aa33e54ada22fa"
+    url "https://api.github.com/repos/harleymckenzie/asc/tarball/v0.1.0"
+    sha256 "9c3c2a4ad674243e2b81b502eaab7c372f02e8c39df949d7c4804733ce7884f1"
   end
 
   resource "urllib3" do
-    url "https://api.github.com/repos/harleymckenzie/asc/tarball/v0.4.3"
-    sha256 "b4fe11d045acdf5e59a82219c05273309e6549085624845464aa33e54ada22fa"
+    url "https://api.github.com/repos/harleymckenzie/asc/tarball/v0.1.0"
+    sha256 "9c3c2a4ad674243e2b81b502eaab7c372f02e8c39df949d7c4804733ce7884f1"
   end
 
   def install
